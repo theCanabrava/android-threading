bc4853d636896b32ebffd6c23df3b326bc4598d7
thecanabrava <vcanabravatr@gmail.com>
2022-08-02 11:28:26 -0300

    Default work and periodic work

---
+++ app/src/main/java/com/example/androidthreading/worker/CustomWorker.kt
@@ -0,0 +1,18 @@
+package com.example.androidthreading.worker
+
+import android.content.Context
+import android.util.Log
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import com.example.androidthreading.process.HeavyProcess
+
+class CustomWorker(appContext: Context, workerParams: WorkerParameters):
+    Worker(appContext, workerParams)
+{
+    private val tag = "CUSTOM_WORKER"
+    override fun doWork(): Result {
+        val result = HeavyProcess.run(10000)
+        Log.d(tag, "Custom worker finished with result: $result")
+        return Result.success()
+    }
+}
--- app/src/main/java/com/example/androidthreading/worker/WorkerActivity.kt
+++ app/src/main/java/com/example/androidthreading/worker/WorkerActivity.kt
@@ -2,11 +2,45 @@ package com.example.androidthreading.worker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.androidthreading.R
+import androidx.work.*
+import com.example.androidthreading.databinding.ActivityWorkerBinding
+import java.util.concurrent.TimeUnit
 
 class WorkerActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityWorkerBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_worker)
+        binding = ActivityWorkerBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.doWork.setOnClickListener { simpleWork() }
+        binding.doPeriodicWork.setOnClickListener { periodicWork() }
+    }
+
+    private fun simpleWork()
+    {
+        val customWorkRequest: WorkRequest =
+            OneTimeWorkRequestBuilder<CustomWorker>()
+                .setExpedited(OutOfQuotaPolicy.RUN_AS_NON_EXPEDITED_WORK_REQUEST)
+                .build()
+        enqueue(customWorkRequest)
+    }
+
+    private fun periodicWork()
+    {
+        val periodicWorkRequest =
+            PeriodicWorkRequestBuilder<CustomWorker>(20, TimeUnit.MINUTES)
+                .build()
+        enqueue(periodicWorkRequest)
+    }
+
+    private fun enqueue(request: WorkRequest)
+    {
+        WorkManager
+            .getInstance(this)
+            .enqueue(request)
     }
 }
